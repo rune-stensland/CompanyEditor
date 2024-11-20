cbc5faa367652c5eecbbbdc17f5b2733651c46d5
sp-hash <amigaguru@gmail.com>
2024-11-20 14:14:29 +0100

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using CompanyEditor.Models;
+
+namespace CompanyEditor.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+        public DbSet<CompanyEditor.Models.Company> Company { get; set; } = default!;
+    }
+}
---
+++ models/Company.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System.ComponentModel.DataAnnotations;
+
+namespace CompanyEditor.Models
+{
+    public class Company
+    {
+        [Key]
+        public int Id    { get; set; }
+        public string? OrgNumber { get; set; }
+        public string? Name { get; set; }
+        public string? Adress { get; set; }
+        public string? AreaCode { get; set; }
+        public string? City { get; set; }
+        public string? EmailAdress { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+}
